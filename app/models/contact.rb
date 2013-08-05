fd68cf22d746e0924750112fe1f4748f4e20c3aa
Joe Gesualdo <joegesualdo@gmail.com>
2013-08-05 10:21:36 -0400

    Pass most tests for Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :firstname, :lastname
+
+  validates :firstname, :lastname, presence: true
+  validates_uniqueness_of :email
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -10,9 +10,28 @@ describe Contact do
     expect(contact).to be_valid
   end
 
-  it "is invalid without a firstname"
-  it "is invalid without a lastname"
-  it "is invalid with a duplicate email address"
+  it "is invalid without a firstname" do
+    expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+  end
+
+  it "is invalid without a lastname" do
+    expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+  end
+
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+        firstname: "Joe",
+        lastname: "Gesualdo",
+        email: "joe@example.com"
+    )
+    contact = Contact.new(
+        firstname: "Joe",
+        lastname: "Shmo",
+        email: "joe@example.com"
+    )
+    expect(contact).to have(1).errors_on(:email)
+  end
+
   it "returns a contact's full name as a string"
 
 end
