e9b05fe73bf82c0ca6b9dd903f0311bf09f166f1
Joe Gesualdo <joegesualdo@gmail.com>
2013-08-05 09:54:39 -0400

    Pass first test: is valid with a firstname and lastname

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,18 @@
+# all of your specs will include this require line moving forward.
 require 'spec_helper'
 
 describe Contact do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a firstname and lastname" do
+    contact = Contact.new(
+        firstname: "Joe",
+        lastname: "Gesualdo",
+        email: "joe@example.com")
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a firstname"
+  it "is invalid without a lastname"
+  it "is invalid with a duplicate email address"
+  it "returns a contact's full name as a string"
+
 end
